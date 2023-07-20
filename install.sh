1b51fdd6aab99873ab5489379984619c667ee4b8
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-07-20 22:58:25 +0800

    remove no use

--- install.sh
+++ install.sh
@@ -2,19 +2,5 @@
 
 uname -a
 
-echo $CREDENTIALS > credentials.json
-
-# install tesseract v5 (without this, ubuntu only has v4)
-sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
-
-sudo apt-get update
-sudo apt-get install -y gifsicle imagemagick tesseract-ocr tesseract-ocr-eng
-
 # extract data from png
 pip install opencv-python-headless numpy pandas
-
-# version check
-convert --version
-tesseract --version
-
-npm i
